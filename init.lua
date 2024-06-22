33dbc6b5236a5b1a2b37847094fa3855ff17f9a8
daniel-kaz <danielk.au03@gmail.com>
2024-06-22 21:11:37 +1000

    Initial commit

---
+++ init.lua
@@ -0,0 +1,1 @@
+require("dankaz")
---
+++ lua/dankaz/init.lua
@@ -0,0 +1,16 @@
+require("dankaz.mappings")
+require("dankaz.lazy_init")
+
+-- options
+
+vim.opt.nu = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.hlsearch = false
+vim.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.updatetime = 50
